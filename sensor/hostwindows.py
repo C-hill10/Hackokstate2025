30184e7f06f69f3536bee617bbebbe89a7e224f4
C-hill10 <osuconnor@live.com>
2025-11-02 11:14:40 -0600

    final part of code, can talk to website

--- sensor/hostwindows.py
+++ sensor/hostwindows.py
@@ -8,27 +8,26 @@ import random
 from SensorClient import SensorClient
 def main():
     #we are assuming that all restaurant capacity is 50 people
-    restaurant = SensorClient("kerr drummond","hackokstate25")
-    numpeople = random.randrange(0,50,1) #generate a random restaurant capacity for testing
-    business=numpeople*2
-    restaurant.send_update(business)
+    restaurant = SensorClient("caf-libro","hackokstate25")
+    numpeople = random.randint(0,50) #generate a random restaurant capacity for testing
+    restaurant.send_update(numpeople*2)
 
     try:
         arduino = serial.Serial(port='COM4',baudrate= 9600,timeout=.1)
         time.sleep(2)  # Wait for Arduino reset
         #GrilledCheese= Restaurant("Cheems",50)
-        restaurant = SensorClient("kerr drummond","hackokstate25")
         while True:
             resp = arduino.read()
             if resp == b'H':
                # GrilledCheese.customer_enters()
-               if random.randrange(0,1,1)==1:
+               if random.randint(0,1)==1:
                     numpeople=numpeople+1
                else: 
                    numpeople=numpeople-1
             business=numpeople*2
+            print(f"Number of people is {numpeople} crowded level is {business}")
             restaurant.send_update(business)
-                
+            time.sleep(1)
                 
 
     except KeyboardInterrupt:
